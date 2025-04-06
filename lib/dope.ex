d314e647a249defc9438f30ccd2cf55bf59b6a20
Lostinaa <Lostinaa@github.com>
2025-04-06 15:26:28 +0300

    Gooot iittt

---
+++ lib/dope.ex
@@ -0,0 +1,13 @@
+defmodule Dope do
+  use Application
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Dope.Worker.start_link(arg)
+      # {Dope.Worker, arg}
+    ]
+
+    opts = [strategy: :one_for_one, name: Dope.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
+dope.
